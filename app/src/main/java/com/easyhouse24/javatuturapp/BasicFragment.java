9616cd43381cf4c2674d72801a5e0c68bf5d0841
MacZidane <zidaneorock@yahoo.fr>
2019-01-28 13:38:51 -0800

    New

---
+++ app/src/main/java/com/easyhouse24/javatuturapp/BasicFragment.java
@@ -0,0 +1,43 @@
+package com.easyhouse24.javatuturapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BasicFragment extends Fragment {
+
+    private CardView mCardViewBasic_15;
+
+    private CardView mCardViewBasic_16;
+
+
+    public BasicFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_basic, container, false);
+
+      /* mCardViewBasic_15 = (CardView)view.findViewById(R.id.CardViewBasic_15);
+        mCardViewBasic_16 = (CardView)view.findViewById(R.id.CardViewBasic_16);*/
+
+
+
+
+
+        return view;
+    }
+
+}
